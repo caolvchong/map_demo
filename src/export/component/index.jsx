d160b377b2557b79c278f3115be483706d0c1c67
任卓 <770809@nd.com>
2019-12-13 15:35:59 +0800

    init by gem-mine 👻

---
+++ src/export/component/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import intl from '@gem-mine/intl'
+import useI18n from '@/export/i18n'
+import Eg from './eg'
+
+export default function eg() {
+  const { isInited } = useI18n()
+  return (
+    isInited &&
+      (<div>
+        <div>
+          {intl.get('welcome')}
+        </div>
+        <Eg />
+      </div>)
+  )
+}
